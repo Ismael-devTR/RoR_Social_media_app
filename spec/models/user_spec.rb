27df2f29a7737cbbb33cd8e5d7fcd641876b6f88
Ismael-devTR <dev.tristan.romero@gmail.com>
2022-01-20 08:57:22 -0600

    Creating TDD for each model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,80 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  def create_a_user(email: "#{SecureRandom.hex(4)}@example.org")
+    User.create!(
+      first_name: "tris",
+      email: email,
+      username: SecureRandom.hex(4)
+    )
+  end
+
+  describe "#valid?" do
+    it 'is valid when email is unique' do
+      create_a_user
+
+      user1 = create_a_user
+      user2 = create_a_user
+
+      expect(user2.email).not_to be user1.email
+      expect(user2).to be_valid
+
+    end
+
+
+    it 'is invalid if the email is taken' do
+      create_a_user(email: "adam@example.org")
+
+      user = User.new
+      user.email = "adam@example.org"
+      expect(user).not_to be_valid
+    end
+
+    it 'is invalid if the username is taken' do
+      user = create_a_user
+      another_user = create_a_user
+
+      expect(another_user).to be_valid
+      another_user.username  = user.username
+      expect(another_user).not_to be_valid
+
+    end
+
+    it "is invalid if users's first_name is blank " do
+      user = create_a_user
+      expect(user).to be_valid
+
+      user.first_name = ''
+      expect(user).not_to be_valid
+
+      user.first_name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is invalid if the email looks bougus' do
+      user = create_a_user
+      expect(user).to be_valid
+
+      user.email = ""
+      expect(user).to be_invalid
+
+      user.email = "foo.bar"
+      expect(user).to be_invalid
+
+      user.email = "foo.bar#example.com"
+      expect(user).to be_invalid
+
+      user.email = "f.o.o.b.a.r@example.com"
+      expect(user).to be_valid
+
+      user.email = "foo+bar@example.com"
+      expect(user).to be_valid
+
+      user.email = "foo.bar@sub.example.com"
+      expect(user).to be_valid
+    end
+
+  end
+
 end
